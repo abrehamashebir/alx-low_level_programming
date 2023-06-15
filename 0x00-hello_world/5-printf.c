2fe017e1b5583baca26c13d1c738b7451de89a8f
abrehamashebir <abrisha96@gmail.com>
2023-06-15 04:55:40 -0700

    printing string literals with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*print results with printf
+ *to print string literals
+ *The entry point to the program
+ */ 
+int main ()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
