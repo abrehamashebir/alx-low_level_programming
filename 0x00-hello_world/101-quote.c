08c64859b1e70a23a1fab5fa964394906b1db217
abrehamashebir <abrisha96@gmail.com>
2023-06-15 07:45:03 -0700

    print in new line

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /*
- * main - The entrance to main
+ * main - Entry point
  *
- * Return - alyways return success
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	printf(STDOUT_FILENO,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
