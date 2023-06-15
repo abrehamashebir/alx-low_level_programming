53c6bf77d67143d5fbd67c27f7f1b1e1f2d8cbb4
abrehamashebir <abrisha96@gmail.com>
2023-06-15 07:38:38 -0700

    print in new line

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - The entrance to main
+ *
+ * Return - alyways return success
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
