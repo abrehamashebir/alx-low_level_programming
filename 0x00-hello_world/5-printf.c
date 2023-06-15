0bad310faa00cd79e83ecf85636397bea36b381a
abrehamashebir <abrisha96@gmail.com>
2023-06-15 05:44:43 -0700

    fix printf in betty stle

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 
 /**
- * print results with printf
- * to print string literals
- *  
- * main Entry point
+ * main - Entry point
  *
- * The entry point to the program
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-        /*print string literals*/
+
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	/*print string literals*/
 	return (0);
 
 }
