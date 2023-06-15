56d32d1df7816d5a498ec462fac71ccfdfb45a7a
abrehamashebir <abrisha96@gmail.com>
2023-06-15 04:58:34 -0700

    printing string literals with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  *to print string literals
  *The entry point to the program
  */ 
-int main ()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
