34d79db8e9cb02e1cc27c41caa4b0c454346b877
abrehamashebir <abrisha96@gmail.com>
2023-06-16 09:00:55 -0700

    print char numbers uing puchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers using putchar function
+ *
+ * Return: always success
+ */
+int main(void)
+{
+	int num;
+	for (num = '0' ; num <= '9' ; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
