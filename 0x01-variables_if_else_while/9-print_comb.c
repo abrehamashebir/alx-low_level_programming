1aaba44cd6279034e72f17d05cb7b5a4da3abc31
abrehamashebir <abrisha96@gmail.com>
2023-06-16 09:37:55 -0700

    print combination of digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,8 +12,12 @@ int main(void)
 	for (num = '0' ; num <= '9' ; num++)
 	{
 		putchar(num);
-		putchar(',');
-		putchar(' ');
+		
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
