3a021f51f10290d93f0fd5504bc09c07a8aa1b3f
abrehamashebir <abrisha96@gmail.com>
2023-06-16 07:41:04 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a text according number
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch < 'z' ; ch++ )
+	{
+		putchar(ch);
+	putchar('\n');
+	}
+	return (0);
+}
