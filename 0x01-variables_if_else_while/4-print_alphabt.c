90df519efa616c5a5f37b29c7f8b843cbf9e80c5
abrehamashebir <abrisha96@gmail.com>
2023-06-16 08:30:50 -0700

    print alphabet except e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,9 +11,9 @@ int main(void)
 
 	for(ch = 'a' ; ch <= 'z' ; ch++)
 	{
-		if(ch == 'e' || ch == 'q')
+		if (ch == 'e' || ch == 'q')
 			continue;
-			putchar(ch);
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
