7983eb48a11b15726a37508a608412ed2dd9da09
abrehamashebir <abrisha96@gmail.com>
2023-06-16 08:05:26 -0700

    print capital and small letters

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,13 +5,12 @@
  *
  * Returrn: always success
  */
-int main (void)
+int main(void)
 {
 	char ch;
 
 	for (ch = 'a' ; ch <= 'z' ;  ch++)
 		putchar(ch);
-	
 	for (ch = 'A' ; ch <= 'Z' ; ch++)
 		putchar(ch);
 
