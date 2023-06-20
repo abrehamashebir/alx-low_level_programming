96006ae3e06e22407a83ffa394c6d9fa12236fdc
abrehamashebir <abrisha96@gmail.com>
2023-06-20 04:56:11 -0700

    print alphabet in small letter

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print putchar
+ *
+ * Return: always success
+ */
+void print_alphabet(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
