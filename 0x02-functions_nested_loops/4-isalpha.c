6a74694b0a2c92f1778ee478d17743d4aa810896
abrehamashebir <abrisha96@gmail.com>
2023-06-20 05:20:46 -0700

    check if a character is digit or alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * isdigit - print if input is digit
+ *
+ * print digits
+ */
+int _isalpha(char c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
