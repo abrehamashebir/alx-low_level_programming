b82987a471f7dfb4d039a3b83dc82db7f9a9fe5b
abrehamashebir <abrisha96@gmail.com>
2023-06-20 11:34:25 -0700

    adding numbers multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print factor of 3 and 5
+ *
+ * Return: return numbers meltiple of 5 and 3
+ */
+int main(void)
+{
+	int num;
+	int sum1 = 0;
+	int sum2 = 0;
+	int sum3 = 0;
+	int sum = 0;
+
+	for (num = 0 ; num < 10 ; num++)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+		{
+			sum1 += num;
+		}
+		else if ( num % 3 == 0)
+		{
+			sum2 += num;
+		}
+		else if (num % 5 == 0)
+		{
+			sum3 += num;
+		}
+	}
+ sum = sum1 + sum2 + sum3;
+ printf("%d\n", sum);
+ return (0);
+}
