444f63d0546e94adca574f7186253c9890ed3ebb
abrehamashebir <abrisha96@gmail.com>
2023-06-20 05:32:12 -0700

    Find the absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,9 @@
+#include "main.h"
+ /* _abs - returns absolute value of num
+ * @n: integer to be tested
+ * Return: absolute value of a number
+ */
+int _abs(int num)
+{
+	return (num < 0 ? -1 * num : num);
+}
