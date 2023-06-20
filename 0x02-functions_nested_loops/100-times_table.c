9e6116f8e7a63006ae54b764614ef20a069766e2
abrehamashebir <abrisha96@gmail.com>
2023-06-20 12:25:47 -0700

    time table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,116 +1,34 @@
 #include "main.h"
 /**
- *powB - raises the number base to power power
- *@base : the base
- *@power : the power
- *Return: return the answer
+ * times_table - prints times table
  */
-int powB(int base, int power)
+void times_table(void)
 {
-	int i, prod = 1;
+	int i, j, prod;
 
-	for (i = 0; i < power; i++)
+	for (i = 0; i < 10; i++)
 	{
-		prod = prod * base;
-	}
-	return (prod);
-}
-
-/**
- *numLength - returns the lenth of string
- *@num : operand number
- *Return: number of digits
- */
-int numLength(int num)
-{
-	int length = 0;
-
-	if (!num)
-	{
-		return (1);
-	}
-
-	while (num)
-	{
-		num = num / 10;
-		length += 1;
-	}
-
-
-	return (length);
-}
-
-/**
- *putnchar - Print a number of any any digit
- *@num : takes an input number
- *
- *
- */
-void putnchar(int num)
-{
-
-	int length = numLength(num), j = length - 1, k, tmp2, digit1;
-
-	if (num == 0)
-		_putchar(48);
-	else
-	{
-
-		while (j >= 0)
+		for (j = 0; j < 10; j++)
 		{
-			if (num % powB(10, j) == 0 && j != 0)
+			if (j == 0)
 			{
-				_putchar(48 + num / powB(10, j));
-				for (k = j; k > 0 ; k--)
-				{
-					_putchar(48);
-				}
-				j = -1;
+				_putchar(48);
+				continue;
+			}
+			prod = i * j;
+			_putchar(',');
+			_putchar(' ');
+			if (prod >= 10)
+			{
+				_putchar(prod / 10 + 48);
+				_putchar(prod % 10 + 48);
 			}
 			else
 			{
-				digit1 = num / powB(10, j);
-				_putchar(digit1 + 48);
-				tmp2 = num;
-				num -= powB(10, j) * digit1;
-				if (numLength(tmp2) - numLength(num) == 2)
-				{
-					_putchar(48);
-					j--;
-				}
-				j--;
+				_putchar(' ');
+				_putchar(prod + 48);
 			}
 		}
-	}
-
-}
-/**
- * sumr - returns the sum of numbers from start to end with step steps
- * @start: the first number
- * @end: the last number
- *@step:the number of  steps
- * Return: sum
- */
-int sumr(int start, int end, int step)
-{
-	int sum = 0, i;
-
-	for (i = start; i < end; i += step)
-	{
-		sum += i;
-	}
-	return (sum);
-
-}
-/**
- * main - prints the sum of all numbers under 1024
- * that are divisible by 3 or 5
- * Return: 0
- */
-int main(void)
-{
-
-	putnchar(sumr(3, 1024, 3) + sumr(5, 1024, 5) - sumr(15, 1024, 15));
 	_putchar('\n');
-	return (0);
+	}
 }
