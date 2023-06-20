1405c10dfe4e7d5b4d755e912edf0c6d95952722
abrehamashebir <abrisha96@gmail.com>
2023-06-20 05:11:55 -0700

    check if alphabet is lower or not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * islower - prints lower case aphabets
+ *
+ * Returns: lowercase
+ */
+int _islower(char c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+
+}
