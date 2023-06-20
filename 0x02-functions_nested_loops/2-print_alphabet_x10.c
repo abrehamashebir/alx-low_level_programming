a825691692054f9028796f0866f111bde2f917e7
abrehamashebir <abrisha96@gmail.com>
2023-06-20 06:59:27 -0700

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,14 @@
  *
  * Return: results
  */
-void print_alphabet_x10 (void)
+void print_alphabet_x10(void)
 {
 	int n=0;
 	char ch;
 
 	while (n < 10)
 	{
-		for (ch ='a' ;ch <= 'z' ; ch++)
+		for (ch = 'a' ; ch <= 'z' ; ch++)
 		{
 			_putchar(ch);
 		}
