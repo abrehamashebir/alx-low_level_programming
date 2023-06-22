dc55889675c3e766ff13cab9dbd6e3bb0b4655b2
abrehamashebir <abrisha96@gmail.com>
2023-06-21 23:50:12 -0700

    print the product of a and b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - print the product of a  and b
+ * @a: first parameter
+ * @b: second parameter
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
