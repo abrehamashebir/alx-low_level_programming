d38efe469c8b30457557ade722f81152a3a1ec41
abrehamashebir <abrisha96@gmail.com>
2023-06-26 01:24:55 -0700

    reseting to 98 to a pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * void reset_to_98 - print pointer value
+ * reset_to_98 - print pointer value
  * @n: first pointer parameter
  * Return: pointer value
  */
 void reset_to_98(int *n)
 {
-	*n=98;
+	*n = 98;
 }
