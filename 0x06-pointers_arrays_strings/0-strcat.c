7fefa522b1c81b0334b74f2ff934dd131211cae5
abrehamashebir <abrisha96@gmail.com>
2023-06-29 02:39:43 -0700

    concantinating pointer strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - concantinate strings
+ * @dest:first parameter
+ * @src: second parameter
+ * Return: no return value
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+	
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0 ; src[j] != '\0' ; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+}
