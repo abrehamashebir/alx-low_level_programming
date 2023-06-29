ae24fe931b448126147f6923813ff49a3f0a99e3
abrehamashebir <abrisha96@gmail.com>
2023-06-29 07:02:17 -0700

    converting to upperwq

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper
+ * str: first parameter
+ * Return: returns upper case value
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0' ; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return str;
+}
