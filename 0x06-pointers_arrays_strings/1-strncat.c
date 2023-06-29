c23900d62a24daa27e0f4c3deaa02a2cc0600351
abrehamashebir <abrisha96@gmail.com>
2023-06-29 04:59:13 -0700

    concating n characters

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncat - print concat values
+ * @dest: parameter that hold concat values
+ * @src: variable to be concat
+ * @n): number of characters
+ * Return: return concat values
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, b;
+
+	a = 0;
+	while ( dest[a] != '\0')
+	{
+		a++;
+	}
+	for (b = 0; b < n && src[b] != '\0'; b++)
+	{
+		dest[a] = src[b];
+		a++;
+	}
+	return (dest);
+}
